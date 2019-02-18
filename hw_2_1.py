8510db1af76626b4d204b3d12e31fe0ab4cb0063
v0lax <47493347+v0lax@users.noreply.github.com>
2019-02-18 07:53:53 +0200

    Add files via upload

--- hw_2_1.py
+++ hw_2_1.py
@@ -5,14 +5,15 @@
     *  according to ISO 80000-2 standard   *
     ****************************************'''
 
-err_msg_invalid = 'Invalid data or negative number'
+#err_msg_invalid = 'Invalid data or negative number'
 
 num1 = input('Enter 1st number greater than or equal to 0: ')
 num1 = num1.replace(' ', '')
 
 if (num1.find('-') != -1) or (not num1.replace('.', '', 1).isdigit()):
-    input(err_msg_invalid)
-    exit()
+    num1 = 0
+#    input(err_msg_invalid)
+#    exit()
 elif num1.replace('.', '', 1).isdigit() and (num1.find('.') != -1):
     num1 = int(num1[:num1.find('.')]) + 1
 else:
@@ -22,32 +23,43 @@ num2 = input('\nEnter 2nd number greater than or equal to 0: ')
 num2 = num2.replace(' ', '')
 
 if (num2.find('-') != -1) or (not num2.replace('.', '', 1).isdigit()):
-    input(err_msg_invalid)
-    exit()
+    num2 = 0
+#    input(err_msg_invalid)
+#    exit()
 elif num2.replace('.', '', 1).isdigit() and (num2.find('.') != -1):
     num2 = int(num2[:num2.find('.')])
 else:
     num2 = int(num2)
 
 # Check
-if num1 >= num2:
-    input('\nThe 2nd number must be greater than the 1st.')
-    exit()
+if num1 > num2:
+    summa = 0
+    for x in range(num2, num1 + 1):
+        summa += x
+    print('\n\nThe sum of natural numbers from {} to {} is {} (using "for")'.format(num2, num1, summa))
 
-# "for"
-summa = 0
-for x in range(num1, num2 + 1):
-    summa += x
+    summa = 0
+    x = num2
+    while x <= num1:
+        summa += x
+        x += 1
+    print('\n\nThe sum of natural numbers from {} to {} is {} (using "while")'.format(num2, num1, summa))
 
-print('\n\nThe sum of natural numbers from {} to {} is {} (using "for")'.format(num1, num2, summa))
+elif num1 == num2:
+    summa =0
+    print('\n\nThe sum of natural numbers from {} to {} is {}'.format(num1, num2, summa))
 
-# "while"
-summa = 0
-x = num1
-while x <= num2:
-    summa += x
-    x += 1
+else:
+    summa = 0
+    for x in range(num1, num2 + 1):
+        summa += x
+    print('\n\nThe sum of natural numbers from {} to {} is {} (using "for")'.format(num2, num1, summa))
 
-print('\n\nThe sum of natural numbers from {} to {} is {} (using "while")'.format(num1, num2, summa))
+    summa = 0
+    x = num1
+    while x <= num2:
+        summa += x
+        x += 1
+    print('\n\nThe sum of natural numbers from {} to {} is {} (using "while")'.format(num1, num2, summa))
 
 input('\nPress Enter to exit')
