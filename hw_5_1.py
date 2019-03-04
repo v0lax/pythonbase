ffc08d480b50c8dc485327b2265faa41313d326d
v0lax <47493347+v0lax@users.noreply.github.com>
2019-03-04 17:31:49 +0200

    Add files via upload

---
+++ hw_5_1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+''' ********************************************
+    *  Parser of numbers from the text string  *
+    *                                          *
+    ********************************************'''
+
+def main():
+    text = "kj5p67lljkjkj8"
+    result = get_digits_from_text(text)
+    print("'{}' => {}".format(text,result))     
+
+
+def get_digits_from_text(string):
+    '''get_digits_from_text(<Text string>)\n\n\
+Returns: numbers in case they in ascending order in text string'''
+
+    class ValueError(Exception):
+        def __init__(self):
+            ValueError.text = 'Ошибка: Недопустимое значение в тексте - пробел.'
+    
+    class IndexError(Exception):
+        def __init__(self):
+            IndexError.text = 'Ошибка: Нарушена последовательность цифр.'
+            
+    
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
+    
+    n1 = None
+    n = None
+    
+    for char_1st in string:
+        try:
+            if char_1st == ' ':
+                raise ValueError
+        except ValueError:
+            #print("'{}' => {}".format(string, ValueError.text))
+            return ValueError.text
+            #exit()
+        if char_1st in numbers:
+            n1 = int(char_1st)
+            result = char_1st
+            break
+    
+    for character in string[string.find(char_1st) + 1:]:
+        try:
+            if character == ' ':
+                raise ValueError
+        except ValueError:
+            return ValueError.text
+        if character in numbers:
+            n = int(character)
+            try:
+                if n - n1 == 1:
+                    result += character
+                    n1 = n
+                else:
+                    raise IndexError
+            except IndexError:
+                return IndexError.text
+    
+    return result
+
+
+if __name__ == '__main__':
+    main()
